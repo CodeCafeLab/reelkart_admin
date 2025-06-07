31c0bbbd8bb8a9d9fdd2c74aceabcd2ab09fb901
CodeCafe lab <codecafelabtechnologies@gmail.com>
2025-06-07 03:10:40 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
+
 import type {NextConfig} from 'next';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const withNextIntl = require('next-intl/plugin')('./src/i18n.ts');
+const path = require('path'); // Import path module
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withNextIntl = require('next-intl/plugin')(
+  path.resolve(__dirname, './src/i18n.ts') // Use absolute path
+);
 
 const nextConfig: NextConfig = {
   /* config options here */
