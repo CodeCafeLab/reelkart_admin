90a1d284cec68353e1a13368dbcfd45b257e87f9
CodeCafe lab <codecafelabtechnologies@gmail.com>
2025-06-09 12:29:26 +0000

    Console Error
    Hydration failed because the server rendered HTML didn't

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -1,19 +1,22 @@
+
 import * as React from "react"
 
 const MOBILE_BREAKPOINT = 768
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
+  const [isMobile, setIsMobile] = React.useState(false); // Default to false for server and initial client render
 
   React.useEffect(() => {
-    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
-    const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    }
-    mql.addEventListener("change", onChange)
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    return () => mql.removeEventListener("change", onChange)
-  }, [])
+    // This function will only run on the client
+    const checkDevice = () => {
+      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
+    };
 
-  return !!isMobile
+    checkDevice(); // Check on mount
+    
+    window.addEventListener("resize", checkDevice);
+    return () => window.removeEventListener("resize", checkDevice);
+  }, []); // Empty dependency array ensures it runs once on mount on the client
+
+  return isMobile;
 }
